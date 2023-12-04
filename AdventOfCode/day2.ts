1d4a9e0fb944b116d71eeedb98c775a9a6f5c382
delyan-kirov <delyan.k.kirov@gmail.com>
2023-12-04 20:17:42 +0200

    Day 4 of AdventOfCode

--- AdventOfCode/day2.ts
+++ AdventOfCode/day2.ts
@@ -7,7 +7,7 @@ const example = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green' + '\n' 
 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue' + '\n' +
 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red' + '\n' +
 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red' + '\n' +
-'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'  
+'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
 
 function parseInput(input: string): string[][][][] {
   function splitStringWithNumber(inputString: string): string[] {
@@ -23,7 +23,7 @@ function parseInput(input: string): string[][][][] {
 }
 
 function solution1(input : string[][][][]) : number {
-  function isGamePossible(input: string[][][]): boolean { 
+  function isGamePossible(input: string[][][]): boolean {
     function isRoundPossible(input : string[][]): boolean{
       let red = 0; var blue = 0; var green = 0
       function switchHelper(pair : string []): void {
@@ -60,7 +60,7 @@ function solution2(input : string[][][][]) : number {
     }
     game.map(round => round.map(game => switchHelper(game)))
     return (red * green * blue)
-  }  
+  }
   return input.map(game => setPower(game))
               .reduce((acc, val) => acc + val)
 }
