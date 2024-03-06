f463c9115aece1a297d5014470883a9acb1c010e
delyan-kirov <delyan.k.kirov@gmail.com>
2024-03-06 11:55:13 +0200

    adding part 2

--- AdventOfCode/Day11/day11.py
+++ AdventOfCode/Day11/day11.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 input_txt = ""
-with open("./input.txt", "r") as f:
+with open("./input1.txt", "r") as f:
     input_txt = f.read()
     f.close()
 
@@ -57,12 +57,101 @@ paths = list((map(lambda x: abs(x[1][0] - x[0][0]) + abs(x[1][1] - x[0][1]), pat
 print(paths)
 print(sum(paths))
 
+newrow = []
+offset = 0
+
 for i, row in enumerate(rows):
-    row += 1
+    newrow.append(row + 1 + offset)
+    offset += 1
+
+# part 2
+
+rows = newrow
 
 print("")
 print(rows)
 print(cols)
 
-# part 2
+newgals = []
+
+for (row, col) in galaxies:
+    off_row = 0
+    off_col = 0
+    for el in cols:
+        if el < col:
+            off_col += 10 ** 6 - 1
+    for el in rows:
+        if el < row:
+            off_row += 10 ** 6 - 1
+            print("here", off_row)
+    row += off_row
+    col += off_col
+    print("this is row: ", off_row)
+    print((row, col))
+    newgals.append((row, col))
+
+galaxies = newgals 
+print(galaxies)
+
+paths = [(x, y) for x in galaxies for y in galaxies if x > y]
+paths = list((map(lambda x: abs(x[1][0] - x[0][0]) + abs(x[1][1] - x[0][1]), paths)))
+print(paths)
+print(sum(paths))
+
+
+print("--------------")
+'''
+2023 Advent of Code - Day 11 (https://adventofcode.com/2023/day/11)
+Solution by Sav Bell (https://github.com/savbell)
+Challenge: Solve every day in a single line of Python code.
+           See full progress at https://github.com/savbell/advent-of-code-one-liners
+'''
+
+# Input files not included in repo per copyright & Eric Wastl's request.
+# https://www.reddit.com/r/adventofcode/wiki/faqs/copyright/inputs/
+# Replace with path to your personal input file if you would like to run the code.
+input_file = 'input1.txt'
+
+# To match the format of input files for the Basilisk.
+q = { 11: open(input_file).read() }
+
+######################### PART 1: MULTI-LINE SOLUTION #########################
+galaxies = [(x, y) for y, row in enumerate(q[11].strip().split()) for x, col in enumerate(row) if col == '#']
+
+max_x = max(galaxies, key=lambda x: x[0])[0]
+max_y = max(galaxies, key=lambda x: x[1])[1]
+
+empty_rows = [y for y in range(max_y+1) if not any([y == g[1] for g in galaxies])]
+empty_cols = [x for x in range(max_x+1) if not any([x == g[0] for g in galaxies])]
+
+galaxies = [(g[0] + len([x for x in empty_cols if x < g[0]]), g[1] + len([y for y in empty_rows if y < g[1]])) for g in galaxies]
+
+shortest_paths = {}
+for g1 in galaxies:
+    for g2 in galaxies:
+        if g1 != g2 and (g2, g1) not in shortest_paths:
+            shortest_paths[(g1, g2)] = abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+            
+print('Day 11 Part 1:',sum(shortest_paths.values()))
+
+########################## PART 1: ONE-LINE SOLUTION ##########################
+
+
+######################## PART 2: MULTI-LINE SOLUTION ##########################
+galaxies = [(x, y) for y, row in enumerate(q[11].strip().split()) for x, col in enumerate(row) if col == '#']
+
+max_x = max(galaxies, key=lambda x: x[0])[0]
+max_y = max(galaxies, key=lambda x: x[1])[1]
+
+empty_rows = [y for y in range(max_y+1) if not any([y == g[1] for g in galaxies])]
+empty_cols = [x for x in range(max_x+1) if not any([x == g[0] for g in galaxies])]
+
+galaxies = [(g[0] + (len([x for x in empty_cols if x < g[0]]) * 999999), g[1] + (len([y for y in empty_rows if y < g[1]]) * 999999)) for g in galaxies]
+
+shortest_paths = {}
+for g1 in galaxies:
+    for g2 in galaxies:
+        if g1 != g2 and (g2, g1) not in shortest_paths:
+            shortest_paths[(g1, g2)] = abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+print('Day 11 Part 2:',sum(shortest_paths.values()))
 
